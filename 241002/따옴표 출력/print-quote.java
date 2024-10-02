2fb36a1faa29ed3a753e10db4e5b291f39801624
At-this-moment <hellhoundheadskull@naver.com>
2024-10-02 15:21:56 +0900

    Title: 따옴표 출력, Time: 79ms, Memory: 8MB, Status: Passed - CodeTree

--- 241002/따옴표 출력/print-quote.java
+++ 241002/따옴표 출력/print-quote.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("He Says \"It\'s a really simple sentence\".");
+        System.out.print("He says \"It\'s a really simple sentence\".");
         // 여기에 코드를 작성해주세요.
     }
 }
