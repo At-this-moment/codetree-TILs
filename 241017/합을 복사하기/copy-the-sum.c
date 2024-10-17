af022ab58cea7566cc9566a912c3cee049ad9af4
At-this-moment <hellhoundheadskull@naver.com>
2024-10-17 14:35:30 +0900

    Title: 합을 복사하기, Time: 2ms, Memory: 0MB, Status: Passed - CodeTree

--- 241017/합을 복사하기/copy-the-sum.c
+++ 241017/합을 복사하기/copy-the-sum.c
@@ -3,15 +3,8 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int a=1,b=2,c=3;
-    int temp1, temp2;
-
-    temp1=a;
-    a = a+b+c;
-
-    temp2=b;
-    b = temp1+b+c;
-
-    c = temp1+temp2+c;
+    
+    a = b = c = a+b+c;
 
     printf("%d %d %d",a,b,c);
     return 0;
