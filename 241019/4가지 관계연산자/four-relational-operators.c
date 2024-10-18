87ad173cdd4b9fe70da1970a579442e75b0535c4
At-this-moment <hellhoundheadskull@naver.com>
2024-10-19 03:00:32 +0900

    Title: 4가지 관계연산자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241019/4가지 관계연산자/four-relational-operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    printf("%d\n",a>=b);
+    printf("%d\n",a>b);
+    printf("%d\n",b>=a);
+    printf("%d\n",b>a);
+
+    return 0;
+}
