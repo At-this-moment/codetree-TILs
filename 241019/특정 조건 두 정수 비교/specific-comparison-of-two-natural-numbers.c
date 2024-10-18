eab6fdd80730bc493a7afa429963d61ef8c931c7
At-this-moment <hellhoundheadskull@naver.com>
2024-10-19 02:16:24 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241019/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int x,y;
+    scanf("%d %d",&x,&y);
+
+    if(x<y){
+        printf("1");
+    }
+    else{
+        printf("0");
+    }
+    if(x==y){
+        printf("1");
+    }
+    else{
+        printf("0");
+    }
+
+    return 0;
+}
