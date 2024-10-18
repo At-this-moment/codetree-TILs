a0e732e09392fd332adba5d399a300e46a482e4b
At-this-moment <hellhoundheadskull@naver.com>
2024-10-19 03:02:36 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241019/4번의 크기 비교/4-time-comparison.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c,d,e;
+    scanf("%d %d %d %d %d", &a,&b,&c,&d,&e);
+
+    printf("%d\n",a>b);
+    printf("%d\n",a>c);
+    printf("%d\n",a>d);
+    printf("%d\n",a>e);
+
+    return 0;
+}
