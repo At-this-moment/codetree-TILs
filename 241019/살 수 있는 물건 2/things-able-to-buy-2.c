0e1667bc8a3b57ef72e43fb602d3e916350e6421
At-this-moment <hellhoundheadskull@naver.com>
2024-10-19 01:36:50 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241019/살 수 있는 물건 2/things-able-to-buy-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m;
+    scanf("%d",&m);
+
+    if(m>=3000){
+        printf("book");
+    }
+    else if(m>=1000){
+        printf("mask");
+    }
+    else if(m>=500){
+        printf("pen");
+    }
+    else{
+        printf("no")
+    }
+
+    return 0;
+}
