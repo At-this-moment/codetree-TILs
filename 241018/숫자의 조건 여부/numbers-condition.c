fefeecfe7434980bbb44b4036cd5f0edc8ddfb49
At-this-moment <hellhoundheadskull@naver.com>
2024-10-18 22:56:05 +0900

    Title: 숫자의 조건 여부, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241018/숫자의 조건 여부/numbers-condition.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d",&a);
+
+    if(a>=113){
+        printf("1");
+    }
+    else{
+        printf("0");
+    }
+
+    return 0;
+}
