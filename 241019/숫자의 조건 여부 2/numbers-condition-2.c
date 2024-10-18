913cb14089ccaa235060e7f8b543162895d8a4d5
At-this-moment <hellhoundheadskull@naver.com>
2024-10-19 01:46:34 +0900

    Title: 숫자의 조건 여부 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241019/숫자의 조건 여부 2/numbers-condition-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d",&a);
+
+    if(a==5){
+        printf("A");
+    }
+    if(a%2==0){
+        printf("B");
+    }
+
+    return 0;
+}
