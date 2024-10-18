baa001e84f442222ca0561791a4fa4b2f3e59d87
At-this-moment <hellhoundheadskull@naver.com>
2024-10-19 03:47:42 +0900

    Title: 숫자의 조건 여부 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241019/숫자의 조건 여부 3/numbers-condition-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d",&a);
+
+    if(a%13==0 || a%19==0){
+        printf("Ture");
+    }
+    else{
+        printf("False");
+    }
+
+    return 0;
+}
