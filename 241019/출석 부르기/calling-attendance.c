4f695a52ed777e7611d302ca601994aa7cca894f
At-this-moment <hellhoundheadskull@naver.com>
2024-10-19 01:15:38 +0900

    Title: 출석 부르기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241019/출석 부르기/calling-attendance.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+
+    if(n==1){
+        printf("John");
+    }
+    if else(n==2){
+        printf("Tom");
+    }
+    if else(n==3){
+        printf("Paul");
+    }
+    else{
+        printf("Vacancy");
+    }
+
+    return 0;
+}
