388cb41d379c708e6ca34b94b3562ccc6617bfa9
At-this-moment <hellhoundheadskull@naver.com>
2024-10-18 22:22:12 +0900

    Title: 세 수의 합과 평균 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241018/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int x,y,z;
+    scanf("%d %d %d",&x,&y,&z);
+    printf("%d\n",x+y+z);
+    printf("%d",(x+y+z)/3);
+    return 0;
+}
