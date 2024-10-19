634b337b932d29fdad3c8c45cc2b26377ffae7ef
At-this-moment <hellhoundheadskull@naver.com>
2024-10-19 22:04:51 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241019/별 n개 출력하기/print-n-stars.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int i=1;
+    scanf("%d", &n);
+
+    while(i<=n){
+        printf("*\n");
+        i++;
+    }
+
+    return 0;
+}
