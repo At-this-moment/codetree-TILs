3ef46ad86d4aef27ae553e0fdb2b764220265a43
At-this-moment <hellhoundheadskull@naver.com>
2024-10-20 01:40:29 +0900

    Title: 문자열 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241020/문자열 출력하기/print-string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1; i<=n; i++){
+        printf("LeebrosCode\n");
+    }
+
+    return 0;
+}
