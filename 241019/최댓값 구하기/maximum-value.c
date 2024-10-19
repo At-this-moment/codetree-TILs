ef788eaee135eb2be46073bcbd43a8ddfbe69580
At-this-moment <hellhoundheadskull@naver.com>
2024-10-19 18:58:58 +0900

    Title: 최댓값 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241019/최댓값 구하기/maximum-value.c
+++ 241019/최댓값 구하기/maximum-value.c
@@ -5,13 +5,13 @@ int main() {
     int a,b,c;
     scanf("%d %d %d",&a,&b,&c);
 
-    if(a>=b && a>=c){
+    if(a>b && a>c){
         printf("%d",a);
     }
-    if(b>=a && b>=c){
+    if(b>a && b>c){
         printf("%d",b);
     }
-    if(c>=a && c>=b){
+    if(c>a && c>b){
         printf("%d",c);
     }
     
