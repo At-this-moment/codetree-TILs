8f33f88049c45795922481f046611793f97e846e
At-this-moment <hellhoundheadskull@naver.com>
2024-10-20 01:20:34 +0900

    Title: n부터 1까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241020/n부터 1까지 숫자 출력/print-n-to-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int i=1;
+    scanf("%d",&i);
+    
+    while(i>=1){
+        printf("%d ",i);
+        i--;
+    }
+
+    return 0;
+}
