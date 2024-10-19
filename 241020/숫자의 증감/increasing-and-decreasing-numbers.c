0a960fe2fa91f4706f5dc4ac40d92413a78c51b5
At-this-moment <hellhoundheadskull@naver.com>
2024-10-20 02:23:43 +0900

    Title: 숫자의 증감, Time: 1102ms, Memory: 80MB, Status: Time Limit Exceed - CodeTree

---
+++ 241020/숫자의 증감/increasing-and-decreasing-numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char c;
+    int n;
+
+    scanf("%c %d",&c,&n);
+
+    if(c=='A'){
+        for(int i=1; i<=n; i++){
+            printf("%d ",i);
+        }
+    }
+    if(c=='D'){
+        for(int i=n; i<=n; i--){
+            printf("%d ",i);
+        }
+    }
+
+    return 0;
+}
