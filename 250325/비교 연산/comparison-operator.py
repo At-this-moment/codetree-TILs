8a96a89b984311eebd7d1114b343f8c6b4b3640e
이현재 <hellhoundheadskull@naver.com>
2025-03-25 22:47:20 +0900

    Title: 비교 연산, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250325/비교 연산/comparison-operator.py
@@ -0,0 +1,31 @@
+a,b=map(int,input().split())
+
+if a>=b:
+    print(1)
+else:
+    print(0)
+
+if a>b:
+    print(1)
+else:
+    print(0)
+
+if b>=a:
+    print(1)
+else:
+    print(0)
+
+if b>a:
+    print(1)
+else:
+    print(0)
+
+if a==b:
+    print(1)
+else:
+    print(0)
+
+if a!=b:
+    print(1)
+else:
+    print(0)
