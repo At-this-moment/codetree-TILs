5c5c523b48f336abf4568000894e3ec3c2999a12
이현재 <hellhoundheadskull@naver.com>
2025-03-26 16:50:45 +0900

    Title: 배열의 값을 3배로, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250326/배열의 값을 3배로/triple-the-values-of-the-array.py
+++ 250326/배열의 값을 3배로/triple-the-values-of-the-array.py
@@ -1,9 +1,10 @@
-arr=[]
+m=[]
 
 for _ in range(3):
     row=list(map(int,input().split()))
-    row [x*3 for x in row]
-    arr.append(row)
+    m.append(row)
 
-for row in arr:
-    print(*row)
+for i in range(3):
+    for j in range(3):
+        print(m[i][j]*3,end=' ')
+print()
